e3f3935ab7d55890a0a3fee27c1e264d4ad3c437
azkarate <azkarate@gmail.com>
2023-06-21 10:23:45 +0200

    First commit

---
+++ testing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# -----------------------------------------------------------------
+# [Azkarate] Testing Script
+#            Just prints a sting on the standard error
+# -----------------------------------------------------------------
+
+echo 'this is a test from Mikel Azkarate-askatsua' >/dev/stderr
+
